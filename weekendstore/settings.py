b62360227ab70905de85dbbbb0a04140da643732
NokRattana <76916086+NokRattana@users.noreply.github.com>
2022-05-03 19:27:45 +0100

    Update settings.py

--- weekendstore/settings.py
+++ weekendstore/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9-8f4u=gh53k57%+010)hj-&7#h%uds24t!3&tkz!c5*t0i&r7'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
